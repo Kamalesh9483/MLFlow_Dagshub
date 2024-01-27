594a3a0c7cd9cf9751b85980edfb7c4b70493b71
Kamalesh <kamalesh9483@gmail.com>
2024-01-27 16:22:09 +0530

    updated README and included Dagshub URI in app.py

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ MLFLOW_TRACKING_URI=https://dagshub.com/Kamalesh9483/MLFlow_Dagshub.mlflow \
 MLFLOW_TRACKING_USERNAME=Kamalesh9483 \
 MLFLOW_TRACKING_PASSWORD=899e45d06f2885b1c0b6be61e4b2658207d1bbb7 \
 python script.py
+
--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pandas as pd
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
